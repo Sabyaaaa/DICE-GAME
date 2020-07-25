a93c1f71476cce16bc3d0da5b6f1b05c2968e63e
sabya <sabya7011@gmail.com>
2020-07-25 20:54:16 +0530

    Displaying the winner

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-//    For image 1
-var randomNumber1 = Math.floor(Math.random() * 6) +1;    //1- 6
+    ///For image 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;    //1- 6
 
 var randomDiceImage1 = "dice" + randomNumber1 + ".png";
 
-var randomImageSource1 = "images/" + randomDiceImage1 ; 
+var randomImageSource1 = "images/" + randomDiceImage1;
 
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource1);
 
-// For image 2
-
+     /// For image 2
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomDiceImage2 = "dice" + randomNumber2 + ".png";
@@ -20,3 +19,14 @@ var randomImageSource2 = "images/" + randomDiceImage2;
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", randomImageSource2);
+
+    ///Displaying the WINNER!
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Wins!!🚩 ";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!!🚩 ";
+}
+else {
+    document.querySelector("h1").innerHTML = "Its a DRAW!";
+}
